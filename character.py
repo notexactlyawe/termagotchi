49ba874800bea9c1032bf596ca7b9e7171bf5550
Cameron MacLeod <cmacleod170@googlemail.com>
2018-09-30 14:31:35 +0100

    Add random_walk
    helper function in character to separate out directions
    get_position -> get_animation since more accurate

--- character.py
+++ character.py
@@ -1,3 +1,5 @@
+import random
+
 class Positions:
     MOUTH_OPEN_LEFT = 0
     MOUTH_OPEN_RIGHT = 1
@@ -5,6 +7,10 @@ class Positions:
     MOUTH_CLOSED_LEFT = 3
     MOUTH_CLOSED_RIGHT = 4
     MOUTH_CLOSED_CENTRE = 5
+
+    LEFT   = [MOUTH_OPEN_LEFT, MOUTH_CLOSED_LEFT]
+    CENTRE = [MOUTH_OPEN_CENTRE, MOUTH_CLOSED_CENTRE]
+    RIGHT  = [MOUTH_OPEN_RIGHT, MOUTH_CLOSED_RIGHT]
 
 class Character:
     def __init__(self, filename, player_name=""):
@@ -16,6 +22,20 @@ class Character:
         with open(filename, 'r') as f:
             all_animations = f.read()
             self.animations_list = all_animations.split('********')
+            self.width = max([len(line) for line in all_animations.split('\n')])
+            self.height = max([len(animation.split('\n')) for animation in self.animations_list])
 
-    def get_position(self, position):
+    def get_animation(self, position):
         return self.animations_list[position]
+
+    def get_animation_by_direction(self, direction=0):
+        """ Gets a random animation for the character facing a direction.
+            direction - integer, if positive then right, negative left and 0 centre
+        """
+        if direction < 0:
+            return self.animations_list[random.choice(Positions.LEFT)]
+        if direction == 0:
+            return self.animations_list[random.choice(Positions.CENTRE)]
+        if direction > 0:
+            return self.animations_list[random.choice(Positions.RIGHT)]
+        raise ValueError("Incorrect direction argument")
--- main.py
+++ main.py
@@ -10,6 +10,22 @@ def get_character_list():
     character_files = ['characters/' + name for name in character_files]
     return character_names, character_files
 
+def random_walk(character, screen, prev_x):
+    movements = [0]
+    if prev_x + character.width + 2 < screen.width - 1:
+        movements.append(2)
+    if prev_x >= 2:
+        movements.append(-2)
+
+    move = random.choice(movements)
+    animation = character.get_animation_by_direction(move)
+
+    new_x = prev_x + move
+
+    screen.place(animation, new_x, 0)
+
+    return new_x
+
 if __name__ == "__main__":
     character_names, character_files = get_character_list()
     print("Which character would you like to play with?")
@@ -24,8 +40,9 @@ if __name__ == "__main__":
 
     s = Screen()
 
+    x = s.width // 2
+
     while True:
-        s.place(character.get_position(random.choice(range(6))), 30, 0)
+        x = random_walk(character, s, x)
         s.draw()
         time.sleep(1)
-
